6c04c2bd5d807ca5d10ecf1c9db71dfc2926cf2f
Jeffrey Ojuederhie <vgauss07@gmail.com>
2025-09-15 23:49:03 -0400

    updates

--- src/data_processing.py
+++ src/data_processing.py
@@ -59,7 +59,8 @@ class DataProcessing:
 
             X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                                 test_size=0.2,
-                                                                random_state=32)
+                                                                random_state=32
+                                                                )
 
             logger.info('Data splitted successfully')
 
